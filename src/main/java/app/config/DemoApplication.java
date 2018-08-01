f656140874601474279e71414433298c1ab77a72
TyCy <tycy1@live.com>
2018-08-01 03:15:59 -0400

    dependency up

--- src/main/java/app/config/DemoApplication.java
+++ src/main/java/app/config/DemoApplication.java
@@ -1,9 +1,9 @@
 package app.config;
 
-import org.h2.tools.Server;
+//import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
@@ -14,16 +14,17 @@ public class DemoApplication {
 	}
 
 	
-	@Bean
-	public Server h2Server() {
-	    Server server = new Server();
-	    try {
-	        server.runTool("-tcp");
-	        server.runTool("-tcpAllowOthers");
-	    } catch (Exception e) {
-	        e.printStackTrace();
-	    }
-	    return server;
-
-	}
+//	@Bean
+//	public Server h2Server() {
+//	    Server server = new Server();
+//	    try {
+//	        server.runTool("-tcp");
+//	        server.runTool("-tcpAllowOthers");
+//	    } catch (Exception e) {
+//	        e.printStackTrace();
+//	    }
+//	    return server;
+//
+//	}
+	
 }
